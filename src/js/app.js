a36d0304864e2eb4d93c2fedc9b241305804b776
ktkrau <ktkrau@gmail.com>
2022-03-28 14:12:11 +0000

    b

--- src/js/app.js
+++ src/js/app.js
@@ -32,7 +32,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastname}</h1>
+          <h1>${variables.name} ${variables.lastname} </h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city} ${variables.country}</h3>
           <ul ${variables.socialMediaPosition}class="position-right">
